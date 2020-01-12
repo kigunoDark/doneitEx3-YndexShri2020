a6f54784164a93e1320e07e228c79737a8991540
KigunoDark <krushenitskii@gmail.com>
2020-01-12 16:10:42 +0300

    in a process

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,7 +1,14 @@
 export enum RuleKeys {
     UppercaseNamesIsForbidden = 'uppercaseNamesIsForbidden',
     BlockNameIsRequired = 'blockNameIsRequired',
-    WarningTExtSizeShouldBeEqual = 'WARNING.TEXT_SIZES_SHOULD_BE_EQUAL'
+    WarningTextSizeShouldBeEqual = 'WARNING.TEXT_SIZES_SHOULD_BE_EQUAL',
+    WarningInvalidButtonSize = 'WARNING.INVALID_BUTTON_SIZE',
+    WarningInvalidPlaceholderSize = 'WARNING.INVALID_PLACEHOLDER_SIZE',
+    WarningInvalidButtonPosition = 'WARNING.INVALID_BUTTON_POSITION',
+    GridTooMuchMarketingBlocks = 'GRID.TOO_MUCH_MARKETING_BLOCKS',
+    TextSeveralH1 = 'TEXT.SEVERAL_H1',
+    TextInvalidH2Position = 'TEXT.INVALID_H2_POSITION',
+    TextInvalidH3Position = 'TEXT.INVALID_H3_POSITION',
 }
 
 export enum Severity {
@@ -15,7 +22,14 @@ export enum Severity {
 export interface SeverityConfiguration {
     [RuleKeys.BlockNameIsRequired]: Severity;
     [RuleKeys.UppercaseNamesIsForbidden]: Severity;
-    [RuleKeys.WarningTExtSizeShouldBeEqual]:Severity;
+    [RuleKeys.WarningTextSizeShouldBeEqual]: Severity;
+    [RuleKeys.WarningInvalidPlaceholderSize]: Severity;
+    [RuleKeys.WarningInvalidButtonSize]: Severity;
+    [RuleKeys.WarningInvalidButtonPosition]: Severity;
+    [RuleKeys.GridTooMuchMarketingBlocks]: Severity;
+    [RuleKeys.TextSeveralH1]: Severity;
+    [RuleKeys.TextInvalidH3Position]: Severity;
+    [RuleKeys.TextInvalidH2Position]: Severity;
 }
 
 export interface ExampleConfiguration {
